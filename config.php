77cb6f6e4d99c40ca7cd0d3251b70aca2242f346
shanmugamgsn <shanmugam_gsn@yahoo.co.in>
2018-10-07 18:42:51 +0530

    Updated config
    $config['base_url']='http://mockstockphp.herokuapp.com/mockstock';
    Removed mockstock

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?
 //have to edit scripts/php/connect.php and backend/connect.php
 $config= array();
-$config['base_url']='http://mockstockphp.herokuapp.com/mockstock';
+$config['base_url']='http://mockstockphp.herokuapp.com';
 $config['db_host']='mockstockphp.herokuapp.com';
 $config['db_username']='root';
 $config['db_password']='alse';
