49b8c1b100a1afb9f8a37cedd3f54bc3ee52dd80
shanmugamgsn <shanmugam_gsn@yahoo.co.in>
2018-10-07 18:39:08 +0530

    Update config.php
    Updated configs for heroku

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?
 //have to edit scripts/php/connect.php and backend/connect.php
 $config= array();
-$config['base_url']='http://localhost/mockstock';
-$config['db_host']='localhost';
+$config['base_url']='http://mockstockphp.herokuapp.com/mockstock';
+$config['db_host']='mockstockphp.herokuapp.com';
 $config['db_username']='root';
 $config['db_password']='alse';
 $config['db_database']='mockstock';
