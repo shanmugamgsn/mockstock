d1151b1bcf976c3947986bc95811cf381dd643df
shanmugamgsn <shanmugam_gsn@yahoo.co.in>
2018-10-09 07:36:55 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 //have to edit scripts/php/connect.php and backend/connect.php
 $config= array();
 $config['base_url']='https://mockstockphp.herokuapp.com';
-$config['db_host']='mockstockphp.herokuapp.com';
-$config['db_username']='root';
-$config['db_password']='alse';
+$config['db_host']='https://www.db4free.net/phpMyAdmin/db_structure.php?db=mockstock';
+$config['db_username']='mockstockadmin';
+$config['db_password']='mockstockadmin';
 $config['db_database']='mockstock';
 ?>
